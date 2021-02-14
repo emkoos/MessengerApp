28f9173dda166b150ce58b60e545c4d5be66fdf3
emil.saracyn <emil.saracyn@gmail.com>
2021-02-14 20:07:17 +0100

    Created Models

---
+++ MessengerApp/Models/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public ICollection<Message> Messages { get; set; }
+        public ICollection<User> Users { get; set; }
+        public ChatType Type { get; set; }
+    }
+
+    public enum ChatType
+    {
+        Room,
+        Private
+    }
+}
