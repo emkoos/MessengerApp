be7d8c0d694a5dcccc622ff51078b9064a362540
emil.saracyn <emil.saracyn@gmail.com>
2021-02-14 20:35:46 +0100

    Created database connection

---
+++ MessengerApp/Data/MessengerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MessengerApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Data
+{
+    public class MessengerDbContext : IdentityDbContext<User>
+    {
+        public MessengerDbContext(DbContextOptions<MessengerDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
--- MessengerApp/Startup.cs
+++ MessengerApp/Startup.cs
@@ -1,6 +1,11 @@
+using MessengerApp.Data;
+using MessengerApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,14 +17,25 @@ namespace MessengerApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MessengerDbContext>(options =>
+            options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<MessengerDbContext>()
+                .AddDefaultTokenProviders();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
